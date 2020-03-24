c76cf3e794e770c0d06a664006b5ecb395857ff8
geva308 <48756212+geva308@users.noreply.github.com>
2020-03-24 22:18:44 +0200

    finishing sign-in  handlsubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { auth } from '../../firebase/firebase.utils'
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -17,10 +18,21 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email,password);
+      this.setState({ email: "", password: "" });
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+
+   
   };
 
   handleChange = event => {
