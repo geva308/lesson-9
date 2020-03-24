567dbb125f86e1e75114158580c47f3774c69b15
geva308 <48756212+geva308@users.noreply.github.com>
2020-03-24 22:09:03 +0200

    store authenticated users to our database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,46 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
-};
+  apiKey: "AIzaSyAF5Dmgx0RsrxpfMgYBXbCGM6JQ9DANFwc",
+  authDomain: "auth-test-6a595.firebaseapp.com",
+  databaseURL: "https://auth-test-6a595.firebaseio.com",
+  projectId: "auth-test-6a595",
+  storageBucket: "auth-test-6a595.appspot.com",
+  messagingSenderId: "871085748954",
+  appId: "1:871085748954:web:8a3c9fea11af7134956be3",
+  measurementId: "G-QT47RGSKQL"
+
+}
+
+export const createUserProfileDocument = async (userAuth,additionalData) =>{
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+
+    } catch(err)
+    {
+      console.log('error creating user',err.message)
+    }
+
+  }
+    return userRef;
+
+}
 
 firebase.initializeApp(config);
 
